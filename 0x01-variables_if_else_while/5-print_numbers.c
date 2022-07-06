115f029498d85ec56621a3fa330d49033741b5c9
NIGANZE Alain <niganzealain@gmail.com>
2022-07-06 13:49:50 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
-/**
- * main - entry point
- *
- * Return: alys return 0
- */
+ #include<stdio.h>                                                                                                                      /**
+*main - entry point
+*
+* Return: always return 0
+*/
 int main(void)
 {
 int a = 0;
-for(; a < 10; a++)
-printf("%d", n);
+for(a = 0; a < 10; i++)
+printf("%d", a);
 putchar('\n');
 return (0);
 }
