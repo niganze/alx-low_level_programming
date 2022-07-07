e0189af172402b9c5823af403bd364c8f6bbcf13
NIGANZE Alain <niganzealain@gmail.com>
2022-07-07 11:39:09 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,35 +1,20 @@
-nclude<stdio.h>                                                                                                                       
-
-/**                                                                                                                                     
+#include <stdio.h>
+/**
+ * maun - entry point
  *
- *  * main - entry point                                                                                                                   
- *
- *   *                                                                                                                                      
- *
- *    * Return: always return 0                                                                                                              
- *
- *     */                                                                                                                                     
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-	char a,                                                                                                                                 
-
-	     for (a = 'a'; a <= 'z'; a++)                                                                                                            
-
-	     {                                                                                                                                       
-
-		     if (a == 'q' || a == 'e')                                                                                                               
-
-		     {                                                                                                                                       
-
-			     continue;                                                                                                                               
-
-		     }                                                                                                                                       
-
-		     putchar (a);                                                                                                                            
-
-		     return (0);                                                                                                                             
-
-	     }       
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+continue;
+else if (alphabet == 'e')
+continue;
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
