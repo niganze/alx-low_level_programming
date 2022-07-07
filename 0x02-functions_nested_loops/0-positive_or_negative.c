9533244f24fd3979dd2a51a859931b35fe482baa
NIGANZE Alain <niganzealain@gmail.com>
2022-07-07 10:31:38 -0700

    Task 0

--- 0x02-functions_nested_loops/0-positive_or_negative.c
+++ 0x02-functions_nested_loops/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  *  main - Entry point
  *
@@ -11,38 +12,13 @@
 int main(void)
 
 {
-
 	int n;
-
-
 
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
 
-
-
-	if (n > 0)
-
-	{
-
-		printf("%i is positive\n", n);
-
-	}
-
-
-
-	else if (n == 0)
-
-	{
-
-		printf("%i is zero\n", n);
-
-	}
-
-
-
-	else
+	if (n < 0)
 
 	{
 
@@ -50,10 +26,20 @@ int main(void)
 
 	}
 
+	else if (n == 0)
 
+	{
+
+		printf("%d is zero\n", n);
+
+	}
+
+	else 
+
+	{
+
+		printf("%d is  positive\n", n);
+
+	}
 
 	return (0);
-
-
-
-}
