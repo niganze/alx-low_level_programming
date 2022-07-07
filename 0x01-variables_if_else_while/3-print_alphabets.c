eeed0f24abfd4683f544e7f94d483115965284b4
NIGANZE Alain <niganzealain@gmail.com>
2022-07-07 11:43:19 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-char a ;
-for (a ='a'; a <= 'z'; a++)
+char a;
+for (a = 'a'; a <= 'z'; a++)
 putchar(a);
-for (a ='A'; a <= 'Z'; a++)
+for (a = 'A'; a <= 'Z'; a++)
 putchar(a);
 putchar ('\n');
 return (0);
